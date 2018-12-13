2cd01c8d4e0f844f9b3e4279a65f4e0c378b2b55
Anil Gadekar <anil-gadekar@vwdomatrix.net>
2018-12-13 11:50:34 +0530

    first commit

---
+++ src/main/java/com/vedomatrix/vyom_IA/VyomIaApplication.java
@@ -0,0 +1,13 @@
+package com.vedomatrix.vyom_IA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VyomIaApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(VyomIaApplication.class, args);
+	}
+}
